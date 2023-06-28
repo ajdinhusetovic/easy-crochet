359d84ae91b57db89d50c60988089ba12f69e090
ajdinhusetovic <ajdinhusetovic@outlook.com>
2023-06-28 12:32:26 +0200

    Initialized project and installed react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+        <h1 className='title'>Hello</h1>
+    </>
+  )
+}
+
+export default App
