9df2a8ef3d3aea07134317e6d19c2a7aeeb6f114
ajdinhusetovic <ajdinhusetovic@outlook.com>
2023-06-28 15:37:15 +0200

    made navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,28 @@
+import { useRef } from "react"
 import { Link } from "react-router-dom"
+import { FaBars, FaTimes } from "react-icons/fa"
+import "./navbar.scss"
 
 export const Navbar = () => {
+    const navRef = useRef()
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive-nav")
+    }
+
     return (
-        <nav>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/login"}>Login</Link>
-        </nav>
+        <header>
+            <h1>EasyCrochet</h1>
+            <nav ref={navRef}>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/login"}>Login</Link>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars />
+            </button>
+        </header>
     )
 }
