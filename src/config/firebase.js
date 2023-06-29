625d7aa8801b2a768cbd0b84fbc59c3ca6ba107e
ajdinhusetovic <ajdinhusetovic@outlook.com>
2023-06-29 22:42:23 +0200

    installed firebase and implemented login with google

---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAGyxLR04tbyiREGzk7nxHN2MXIkb5UgWk",
+    authDomain: "easy-crochet-9a792.firebaseapp.com",
+    projectId: "easy-crochet-9a792",
+    storageBucket: "easy-crochet-9a792.appspot.com",
+    messagingSenderId: "1068147101988",
+    appId: "1:1068147101988:web:ef7967025c047b354763b1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider()
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,17 @@
-
+import { auth, provider } from "../config/firebase"
+import { signInWithPopup } from "firebase/auth"
 
 export const Login = () => {
+
+    const signInWithGoogle = async () => {
+        const result = await signInWithPopup(auth, provider)
+        console.log(result)
+    }
     return (
         <section className="login-wrapper">
             <div className="login">
-                <h1>Login</h1>
+                <h1>Sign In With Google To Continue</h1>
+                <button onClick={signInWithGoogle}>Sign In With Google</button>
             </div>
         </section>
     )
