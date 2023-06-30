7726638a319ceb804f943042542ef2dd913d8bcb
ajdinhusetovic <ajdinhusetovic@outlook.com>
2023-06-30 14:19:02 +0200

    added login functionality with email and password

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,21 @@ import { useRef } from "react"
 import { Link } from "react-router-dom"
 import { FaBars, FaTimes } from "react-icons/fa"
 import "./navbar.scss"
+import { auth } from "../config/firebase"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { signOut } from "firebase/auth"
 
 export const Navbar = () => {
     const navRef = useRef()
+    const [user] = useAuthState(auth)
 
     const showNavbar = () => {
         navRef.current.classList.toggle("responsive-nav")
+    }
+
+    // sign out function
+    const logOut = async () => {
+        await signOut(auth)
     }
 
     return (
@@ -16,6 +25,12 @@ export const Navbar = () => {
             <nav ref={navRef}>
                 <Link to={"/"}>Home</Link>
                 <Link to={"/login"}>Login</Link>
+                {user && (
+                    <>
+                        {user?.email}
+                        <button onClick={logOut}>Logout</button>
+                    </>
+                )}
                 <button className="nav-btn nav-close-btn" onClick={showNavbar}>
                     <FaTimes />
                 </button>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,30 @@
-import { auth, provider } from "../config/firebase"
+import { useState } from "react"
+import { auth } from "../config/firebase"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
 
 export const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-    const signInWithGoogle = async () => {
-        const result = await signInWithPopup(auth, provider)
-        console.log(result)
+    // register function
+    const createUserAccount = async () => {
+        await createUserWithEmailAndPassword(auth, email, password)
     }
+
+    // sign in function
+    const signInUser = async () => {
+        await signInWithEmailAndPassword(auth, email, password)
+    }
+
+
     return (
         <section className="login-wrapper">
             <div className="login">
-                <h1>Login</h1>
+                <h1>Create An Account or Sign In With Existing Account</h1>
+                <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={createUserAccount}>Register</button>
+                <button onClick={signInUser}>Sign In</button>
             </div>
         </section>
     )
