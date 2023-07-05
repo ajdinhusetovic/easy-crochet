36247a8707b000893ce247655e90d5d2703a22f3
ajdinhusetovic <ajdinhusetovic@outlook.com>
2023-07-05 23:11:36 +0200

    create register form validation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,30 +1,54 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { auth } from "../config/firebase"
 import { createUserWithEmailAndPassword } from "firebase/auth"
+import { useForm } from 'react-hook-form'
+import * as yup from "yup"
+import { yupResolver } from "@hookform/resolvers/yup"
 import "./register.scss"
 
 export const Register = () => {
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
 
-    const createUserAccount = async () => {
-        await createUserWithEmailAndPassword(auth, email, password)
+    const schema = yup.object().shape({
+        email: yup.string().email().required("This field is required"),
+        password: yup.string().min(4).max(20).required("Password field is required"),
+        passwordConfirm: yup.string().oneOf([yup.ref("password"), null], "Passwords don't match").required("This is a required field"),
+    })
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    })
+
+    const createUserAccount = async (data) => {
+        try {
+            await createUserWithEmailAndPassword(auth, data.email, data.password)
+        } catch (error) {
+            setError("Oops! There's been an error registering your account!")
+        }
     }
 
-  return (
-    <section className="register-wrapper">
+    return (
+        <section className="register-wrapper">
             <div className="register">
                 <h1>Create An Account</h1>
-                <div className="inputs">
-                    <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-                    <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                <div className="buttons">
-                    <button onClick={createUserAccount}>Register</button>
-                </div>
+                <form onSubmit={handleSubmit(createUserAccount)}>
+                    <div className="inputs">
+                        {error && (
+                            <p className='error-message'>{error}</p>
+                        )}
+                        <input placeholder="Email" {...register("email")} />
+                        <p className='error-message'>{errors.email?.message}</p>
+                        <input type="password" placeholder="Password" {...register("password")} />
+                        <p className='error-message'>{errors.password?.message}</p>
+                        <input type="password" placeholder='Confirm Password' {...register("passwordConfirm")} />
+                        <p className='error-message'>{errors.passwordConfirm?.message}</p>
+                    </div>
+                    <div className="buttons">
+                        <button type='submit'>Register</button>
+                    </div>
+                </form>
             </div>
         </section>
-  )
+    )
 }
